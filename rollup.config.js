c6f5946d1ad10b9289269d7c754968b9d8c8a902
jafayer <github@fayer.me>
2024-05-21 20:09:18 -0400

    Changed rollup to cjs compile

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/bundle.js',
-    format: 'esm',
+    format: 'cjs',
     sourcemap: true,
   },
   plugins: [
     typescript(),
+    commonjs(),
     terser(), // Minifies the bundle
   ],
 };
